c94d1a44b75f760b97a2d6da368de94c82f56641
Pizzonia17 <antoniogerardi123@gmail.com>
2024-04-19 16:10:36 +0200

    main edit

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("hello world")
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+#cattura video
+cap = cv2.VideoCapture(0) 
+## Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        #cv2 cattura frame in BGR e non RGB
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+        
+        #print(type(results.pose_landmarks), results.pose_landmarks)
+
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        #extract landmarks 
+        try:
+            landmarks = results.pose_landmarks.landmark
+            print(type(landmarks),landmarks)  
+        except:
+            pass
+
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        #crea popup con il video
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
